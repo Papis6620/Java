5793e27573bf110e3706974115529b7235472cbc
Patryk Krzywda <patryk.krzywda32@gmail.com>
2024-04-29 21:10:58 +0200

    V1.1

--- src/MazeGenerator.java
+++ src/MazeGenerator.java
@@ -1,22 +1,40 @@
 import javax.swing.*;
-import java.awt.*;
 import java.util.*;
 
-public class MazeGenerator extends JPanel{
-    static int mazeHeight = 10, mazeWidth = 10;
-    static int CELL_SIZE = 30;
-    static int[][] maze = new int[mazeHeight][mazeWidth]; // 0 -unvisited, 1-top, 2-right, 3- bottom, 4-left
+public class MazeGenerator {
+    static int mazeHeight, mazeWidth, CELL_SIZE;
+    static int[][] maze;
     static Stack<Map<Integer, Integer>> stack = new Stack<>();
     static int visited = 1;
     static int currentX = 0, currentY = 0;
 
+    public static void main(String[] args) {
+        mazeWidth = Integer.parseInt(JOptionPane.showInputDialog("Enter maze width:"));
+        mazeHeight = Integer.parseInt(JOptionPane.showInputDialog("Enter maze height:"));
+        CELL_SIZE = Integer.parseInt(JOptionPane.showInputDialog("Enter cell size(>= 30):"));
+
+        // Initialize maze array
+        maze = new int[mazeHeight][mazeWidth];
+        JFrame frame = new JFrame();
+        MazePanel mazePanel = new MazePanel();
+        frame.add(mazePanel);
+        frame.setSize(mazeWidth * CELL_SIZE + 17, mazeHeight * CELL_SIZE + 40);
+        frame.setTitle("Maze generator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        setStartValues();
+        generateMaze(mazePanel);
+    }
+
     static void setStartValues() {
         Map<Integer, Integer> startPosition = new HashMap<>();
         startPosition.put(currentX, currentY);
-        maze[currentY][currentX] = 1;
+        maze[currentY][currentX] = MazeCell.TOP;
         stack.push(startPosition);
     }
-    static void generateMaze(MazeGenerator mazePanel) {
+
+    static void generateMaze(MazePanel mazePanel) {
         while (visited < mazeHeight * mazeWidth) {
             Vector<Integer> neighbors = new Vector<>();
             Map<Integer, Integer> position;
@@ -43,19 +61,19 @@ public class MazeGenerator extends JPanel{
                 switch (next) {
                     case 0:
                         currentX += 1;
-                        direction = 4;
+                        direction = MazeCell.LEFT;
                         break;
                     case 1:
                         currentY += 1;
-                        direction = 1;
+                        direction = MazeCell.TOP;
                         break;
                     case 2:
                         currentX -= 1;
-                        direction = 2;
+                        direction = MazeCell.RIGHT;
                         break;
                     case 3:
                         currentY -= 1;
-                        direction = 3;
+                        direction = MazeCell.BOTTOM;
                         break;
                     default:
                         break;
@@ -73,56 +91,7 @@ public class MazeGenerator extends JPanel{
                 Thread.sleep(100); // Pause for 100 milliseconds for better visualization
             } catch (InterruptedException e) {
                 e.printStackTrace();
-        }
-        }
-    }
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, mazeWidth * CELL_SIZE, mazeHeight * CELL_SIZE); // Fill the entire maze with black
-
-        for (int i = 0; i < mazeHeight; i++) {
-            for (int j = 0; j < mazeWidth; j++) {
-                if (maze[i][j] != 0) {
-                    g.setColor(Color.WHITE);
-                    g.fillRect(j * CELL_SIZE + 1, i * CELL_SIZE + 1, CELL_SIZE-2, CELL_SIZE-2); // Fill in path with white
-                    g.setColor(Color.BLACK);
-                    if (i == currentY && j == currentX) {
-                        g.setColor(Color.RED);
-                        g.fillOval(j * CELL_SIZE + 10, i * CELL_SIZE + 10, CELL_SIZE-20, CELL_SIZE-20); // Example: draw a red circle
-                        g.setColor(Color.BLACK);
-                    }
-                    if(maze[i][j] == 1 && i != 0){
-                        g.setColor(Color.WHITE);
-                        g.fillRect((j * CELL_SIZE)+1, (i * CELL_SIZE) -2, CELL_SIZE-2, 3);
-                    }
-                    if(maze[i][j] == 2){
-                        g.setColor(Color.WHITE);
-                        g.fillRect((j * CELL_SIZE)+CELL_SIZE-1, (i * CELL_SIZE)+1 , 3, CELL_SIZE-2);
-                    }
-                    if(maze[i][j] == 3){
-                        g.setColor(Color.WHITE);
-                        g.fillRect((j * CELL_SIZE)+1, (i * CELL_SIZE) +CELL_SIZE-1, CELL_SIZE-2, 3);
-                    }
-                    if(maze[i][j] == 4){
-                        g.setColor(Color.WHITE);
-                        g.fillRect((j * CELL_SIZE)-1, (i * CELL_SIZE) +1, 2, CELL_SIZE-2);
-                    }
-                }
             }
         }
-    }
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        MazeGenerator mazePanel = new MazeGenerator();
-        frame.add(mazePanel);
-        frame.setSize(mazeWidth * CELL_SIZE + 17, mazeHeight*CELL_SIZE + 40);
-        frame.setTitle("Maze generator");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-        setStartValues();
-        generateMaze(mazePanel);
     }
 }
